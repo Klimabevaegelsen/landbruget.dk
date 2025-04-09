892213a324cc86f55c8da289e38c48116ed96051
aleksanderbl29 <github@aleksanderbl.dk>
2025-04-09 14:27:55 +0200

    Commit mage things
    will be removed

---
+++ backend/mage/backend/mage/data_loaders/gødningsregnskab_2023.py
@@ -0,0 +1,31 @@
+from mage_ai.io.file import FileIO
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@data_loader
+def load_data_from_file(*args, **kwargs):
+    """
+    Template for loading data from filesystem.
+    Load data from 1 file or multiple file directories.
+
+    For multiple directories, use the following:
+        FileIO().load(file_directories=['dir_1', 'dir_2'])
+
+    Docs: https://docs.mage.ai/design/data-loading#fileio
+    """
+    #filepath = 'data/godning_2023.xlsx'
+
+    #return FileIO().load(filepath)
+
+    return FileIO().load('default_repo/data_loaders/data/godningsregnskaber_2023.xlsx', format = "excel")
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
