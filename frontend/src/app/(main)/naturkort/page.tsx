ddeced67600942d5dc482718ea74cd6193cf3d1f
Chresten Søndergaad <crsondergaard@gmail.com>
2025-07-10 13:12:09 +0200

    Joined Naturkort and CriteriaSelector

--- frontend/src/app/(main)/naturkort/page.tsx
+++ frontend/src/app/(main)/naturkort/page.tsx
@@ -1,18 +1,11 @@
-import { UdtagningsKort, CriteriaSelector } from "@/components/udtagningskort/udtagningskort";
+import { NaturKortPage } from "@/components/naturkort/naturkort";
 import { Container } from "@/components/layout/container";
 
 export default function UdtagningsKortPage() {
     return (
         <Container>
             <h1 className="text-2xl font bold mb-4">Effektiv udtagning af landbrugsjord</h1>
-            <div className="grid grid-cols-5 gap-2">
-                <div className="col-span-4">
-                    <UdtagningsKort />
-                </div>
-                <div className="col-span-1">
-                    <CriteriaSelector />
-                </div>
-            </div>
+            <NaturKortPage />
         </Container>       
     );
 }
